a37a0f9572bb777f54b7c3a29633f60e281d16d5
Kelly Gravelyn <kelly@kellyrenee.me>
2021-11-06 20:10:04 -0700

    building lots of icons

--- build/src/main.rs
+++ build/src/main.rs
@@ -4,6 +4,27 @@ use tiny_skia;
 use resvg;
 use json;
 
+fn build_dir() -> PathBuf {
+    let exe_path = env::current_exe().ok().unwrap();
+    return exe_path
+        .parent().unwrap()
+        .parent().unwrap()
+        .parent().unwrap()
+        .to_path_buf();
+}
+
+fn theme_dir() -> PathBuf {
+    let exe_path = env::current_exe().ok().unwrap();
+    let mut path = exe_path
+        .parent().unwrap()
+        .parent().unwrap()
+        .parent().unwrap()
+        .parent().unwrap()
+        .to_path_buf();
+    path.push("theme");
+    return path;
+}
+
 fn json_from_file(build_dir: &Path, name: &Path) -> json::JsonValue {
     let mut full_path = PathBuf::new();
     full_path.push(build_dir);
@@ -15,37 +36,46 @@ fn json_from_file(build_dir: &Path, name: &Path) -> json::JsonValue {
 }
 
 fn main() {
-    // Maybe not the best way but :shrug:
-    let exe_path = env::current_exe().ok().unwrap();
-    let build_dir = exe_path
-        .parent().unwrap()
-        .parent().unwrap()
-        .parent().unwrap();
+    let build_dir = build_dir();
+    let theme_dir = theme_dir();
 
-    let icons = json_from_file(build_dir, Path::new("icons.json"));
+    let _colors = json_from_file(&build_dir, Path::new("colors.json"));
+    let icons = json_from_file(&build_dir, Path::new("icons.json"));
+    let _sizes = json_from_file(&build_dir, Path::new("sizes.json"));
 
-    println!("{}", json::stringify_pretty(icons, 2));
+    for kvp in icons.entries() {
+        let mut full_icon_path = PathBuf::new();
+        full_icon_path.push(&build_dir);
+        full_icon_path.push("assets");
+        full_icon_path.push(format!("{}.svg", kvp.0));
 
-    // let mut opt = usvg::Options::default();
+        let mut full_output_path = PathBuf::new();
+        full_output_path.push(&theme_dir);
+        full_output_path.push(format!("{}.png", kvp.0));
 
-    // // Get file's absolute directory.
-    // opt.resources_dir = std::fs::canonicalize(&args[1]).ok().and_then(|p| p.parent().map(|p| p.to_path_buf()));
+        println!("Building {} to {}", full_icon_path.display(), full_output_path.display());
 
-    // let svg_data = std::fs::read(&args[1]).unwrap();
+        let svg_data = match std::fs::read(&full_icon_path) {
+            Ok(v) => v,
+            Err(e) => panic!("Failed to read icon file {} ({})", full_icon_path.display(), e),
+        };
 
-    // let s = match str::from_utf8(&svg_data) {
-    //     Ok(v) => v,
-    //     Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
-    // };
+        let s = match str::from_utf8(&svg_data) {
+            Ok(v) => v,
+            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+        };
 
-    // // colorize the absolute hack way
-    // let svg_data = s.replace("#000", "#aa9bc1");
+        // colorize the absolute hack way
+        let svg_data = s.replace("#000", "#aa9bc1");
 
-    // let rtree = usvg::Tree::from_data(svg_data.as_bytes(), &opt.to_ref()).unwrap();
+        let opt = usvg::Options::default();
+        let rtree = usvg::Tree::from_data(svg_data.as_bytes(), &opt.to_ref()).unwrap();
 
-    // // TODO: render at 16, 32 (@2x suffix), and 48 (@3x suffix)
-    // let pixmap_size = rtree.svg_node().size.to_screen_size();
-    // let mut pixmap = tiny_skia::Pixmap::new(pixmap_size.width(), pixmap_size.height()).unwrap();
-    // resvg::render(&rtree, usvg::FitTo::Original, pixmap.as_mut()).unwrap();
-    // pixmap.save_png(&args[2]).unwrap();
+        // TODO: render at 16, 32 (@2x suffix), and 48 (@3x suffix)
+        let pixmap_size = rtree.svg_node().size.to_screen_size();
+        let mut pixmap = tiny_skia::Pixmap::new(pixmap_size.width(), pixmap_size.height()).unwrap();
+        resvg::render(&rtree, usvg::FitTo::Original, pixmap.as_mut()).unwrap();
+
+        pixmap.save_png(full_output_path).unwrap();
+    }
 }
